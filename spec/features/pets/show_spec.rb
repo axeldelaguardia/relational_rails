2d47f6e2d4eacd0410b0eb60bd41c0f2514f6e47
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-02-04 13:07:00 -0700

    Add new pet to dog groomer feature

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -3,16 +3,24 @@ require 'rails_helper'
 describe 'Pets Show Page' do
 	describe 'As a visitor' do
 		describe 'when I visit /pets/:id' do
+			before do
+				@dog_groomer1 = DogGroomer.create!(name: 'Malia Kainoa', master_groomer: true, salon: 'LBD', rating: 5)
+				@dog = @dog_groomer1.pets.create!(name: 'Koa', age: 9, weight: 14, trained: true)
+			end
+
 			it 'I see the name of pet with id including attributes' do
-				dog_groomer1 = DogGroomer.create!(name: 'Malia Kainoa', master_groomer: true, salon: 'LBD', rating: 5)
-				dog = dog_groomer1.pets.create!(name: 'Koa', age: 9, weight: 14, trained: true)
+				visit "/pets/#{@dog.id}"
 
-				visit "/pets/#{dog.id}"
+				expect(page).to have_content(@dog.name)
+				expect(page).to have_content("Age: #{@dog.age}")
+				expect(page).to have_content("Weight: #{@dog.weight}")
+				expect(page).to have_content("Trained: #{@dog.trained}")
+			end
 
-				expect(page).to have_content(dog.name)
-				expect(page).to have_content("Age: #{dog.age}")
-				expect(page).to have_content("Weight: #{dog.weight}")
-				expect(page).to have_content("Trained: #{dog.trained}")
+			it 'I see a link to update the pet' do
+				visit "/pets/#{@dog.id}"
+
+				expect(page).to have_link("/pets/#{@dog.id}/edit")
 			end
 		end
 	end
