c38170beba1f62bee59012418b529c7cf56f3772
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-02-05 14:43:43 -0700

    Add default values to update dog groomer or pet

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,14 @@
 DogGroomer.destroy_all
 Pet.destroy_all
 
-groomer1 = DogGroomer.create!( name: 'Groomer 1', master_groomer: true, salon: 'Salon 1', rating: 5 )
-groomer2 = DogGroomer.create!( name: 'Groomer 2', master_groomer: true, salon: 'Salon 2', rating: 4 )
-groomer3 = DogGroomer.create!( name: 'Groomer 3', master_groomer: true, salon: 'Salon 2', rating: 3 )
-groomer4 = DogGroomer.create!( name: 'Groomer 4', master_groomer: false, salon: 'Salon 1', rating: 5 )
-pet1 = Pet.create!( dog_groomer: groomer1, name: 'Fido', age: '5', weight: 17, trained: true )
+groomer1 = DogGroomer.create!( name: 'Rachel Hernandez', master_groomer: true, salon: 'Blue Pooch', rating: 5 )
+groomer2 = DogGroomer.create!( name: 'Cassie Davidson', master_groomer: true, salon: 'The Regal Beagle', rating: 4 )
+groomer3 = DogGroomer.create!( name: 'Kaitlyn Moore', master_groomer: true, salon: 'Coats and Tails', rating: 3 )
+groomer4 = DogGroomer.create!( name: 'Robert Kim', master_groomer: false, salon: 'Coats and Tails', rating: 5 )
+pet1 = Pet.create!( dog_groomer: groomer1, name: 'Django', age: '5', weight: 17, trained: true )
 pet2 = groomer2.pets.create!( name: 'Spot', age: '13', weight: 120, trained: true)
-pet3 = groomer2.pets.create!( name: 'Koa', age: '13', weight: 17, trained: true)
-pet4 = groomer1.pets.create!( name: 'Taja', age: '13', weight: 8, trained: false)
-pet5 = groomer3.pets.create!( name: 'Sushi', age: '13', weight: 34, trained: true )
-pet6 = groomer3.pets.create!( name: 'Milo', age: '13', weight: 123, trained: false )
-pet7 = groomer4.pets.create!( name: 'Buck', age: '13', weight: 66, trained: false )
-
-# rails db:{drop,create,migrate,seed}
+pet3 = groomer2.pets.create!( name: 'Koa', age: '9', weight: 17, trained: true)
+pet4 = groomer1.pets.create!( name: 'Taja', age: '6', weight: 8, trained: false)
+pet5 = groomer3.pets.create!( name: 'Sushi', age: '3', weight: 34, trained: true )
+pet6 = groomer3.pets.create!( name: 'Milo', age: '14', weight: 123, trained: false )
+pet7 = groomer4.pets.create!( name: 'Rupert', age: '1', weight: 66, trained: false )
